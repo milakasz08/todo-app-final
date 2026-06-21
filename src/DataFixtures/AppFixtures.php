822fdde438c1ac29c9a7aec2fbfaf2a1173f63a6
Milena Kaszowska <milakasz08@gmail.com>
2026-06-21 11:58:07 +0000

    Dodanie AppFixtures z uzytkownikami

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,8 +6,10 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 /**
  * Class AppFixtures.
@@ -15,14 +17,38 @@ use Doctrine\Persistence\ObjectManager;
 class AppFixtures extends Fixture
 {
     /**
+     * Constructor.
+     *
+     * @param UserPasswordHasherInterface $passwordHasher
+     */
+    public function __construct(private UserPasswordHasherInterface $passwordHasher)
+    {
+    }
+
+    /**
      * Load the fixtures.
      *
      * @param ObjectManager $manager
+     *
+     * @return void
      */
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        // 1. Tworzenie administratora
+        $admin = new User();
+        $admin->setEmail('admin1@gmail.com');
+        $admin->setRoles(['ROLE_ADMIN']);
+        $admin->setPassword($this->passwordHasher->hashPassword($admin, 'haslo123'));
+        $admin->setIsVerified(true);
+        $manager->persist($admin);
+
+        // 2. Tworzenie zwykłego użytkownika
+        $user = new User();
+        $user->setEmail('user1@gmail.com');
+        $user->setRoles(['ROLE_USER']);
+        $user->setPassword($this->passwordHasher->hashPassword($user, 'haslo1234'));
+        $user->setIsVerified(true);
+        $manager->persist($user);
 
         $manager->flush();
     }
